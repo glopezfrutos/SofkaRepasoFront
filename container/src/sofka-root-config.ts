54e60de97c2a07e4c2b81b9aae0805d0735b5de9
Marlon Vargas <mardrex88@gmail.com>
2022-06-17 13:52:16 -0500

    React -- Se implementan dos Micr-Front basados en react

--- container/src/sofka-root-config.ts
+++ container/src/sofka-root-config.ts
@@ -10,12 +10,22 @@ registerApplication({
   activeWhen: ["/"],
 });
 */
-
+ registerApplication({
+  name: "@Sofka/mf-header",
+  app: () => System.import("@Sofka/mf-header"),
+  activeWhen: ["/"]
+});
+registerApplication({
+ name: "@Sofka/mf-content",
+ app: () => System.import("@Sofka/mf-content"),
+ activeWhen: ["/"]
+});
  registerApplication({
    name: "@Sofka/mf-footer",
    app: () => System.import("@Sofka/mf-footer"),
    activeWhen: ["/"]
  });
+
 // registerApplication({
 //   name: "@Sofka/navbar",
 //   app: () => System.import("@Sofka/navbar"),
