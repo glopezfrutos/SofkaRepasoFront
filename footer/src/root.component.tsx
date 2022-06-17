b3e5ac3108df186525920f43aa951ad8d58550de
jpabloallin <jpac.647@gmail.com>
2022-06-17 12:48:00 -0500

    API rick and morty

--- footer/src/root.component.tsx
+++ footer/src/root.component.tsx
@@ -1,3 +1,41 @@
-export default function Root(props) {
-  return <section>{props.name} FOOTER is mounted!</section>;
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import './App.css';
+
+export default function Root() {
+  
+  const [characters, setCharacters] = useState([])
+  const [query, setQuery] = useState("")
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const {data} = await axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`)
+                setCharacters(data.results)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchData()
+    }, [query])
+
+  return <div className="App">
+            <div className="search">
+                <input type="text"
+                       placeholder={"Search Character"}
+                       className={"input"}
+                       onChange={event => setQuery(event.target.value)}
+                       value={query}
+                />
+            </div>
+            <div className="results">
+                {characters.map(character => (
+                    <div>
+                        <img src={character.image} alt={character.name}/>
+                        {character.name}
+                    </div>
+                ))}
+            </div>
+        </div>;
 }
