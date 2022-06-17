b91807627be9f558cbc85b014b07974e5bbbb3d7
jpabloallin <jpac.647@gmail.com>
2022-06-17 00:45:50 -0500

    Applied react mf on the container.

--- container/src/sofka-root-config.ts
+++ container/src/sofka-root-config.ts
@@ -1,16 +1,17 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
+  name: "@sofka/footer",
+  app: () => System.import("@sofka/footer"),
+  activeWhen: ["/"],
+});
+
+registerApplication({
   name: "@sofka/header",
   app: () => System.import("@sofka/header"),
   activeWhen: ["/"],
 });
 
-registerApplication({
-  name: "@sofka/footer",
-  app: () => System.import("@sofka/footer"),
-  activeWhen: ["/"],
-});
 
 start({
   urlRerouteOnly: true,
